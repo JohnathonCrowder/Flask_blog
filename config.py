62565bac1f2612dda337a65a92f9b657e2215ae3
Johnathon Crowder <139363360+JohnathonCrowder@users.noreply.github.com>
2024-12-05 19:48:07 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://flask_blog_database_wga3_user:guJZJtM65lfBcbRY40c7dYAckBGnBfuP@dpg-ct925td6l47c73am13j0-a.ohio-postgres.render.com/flask_blog_database_wga3'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://flask_blog_database_wga3_user:guJZJtM65lfBcbRY40c7dYAckBGnBfuP@dpg-ct925td6l47c73am13j0-a/flask_blog_database_wga3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join('static', 'uploads')
     MAX_CONTENT_LENGTH = 200 * 1024 * 1024  # 10MB max file size
